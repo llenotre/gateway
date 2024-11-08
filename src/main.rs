4b3cb24234c4a8d127134071009b350999e1b8b2
llenotre <llenotre@student.42.fr>
2024-11-08 22:20:09 +0100

    feat: rate limiting

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,8 @@ use std::time::Duration;
 use tokio::select;
 use tokio::time::interval;
 use tokio_postgres::NoTls;
+use tower_governor::governor::GovernorConfigBuilder;
+use tower_governor::GovernorLayer;
 use tower_http::trace::TraceLayer;
 use tracing::{error, info, warn};
 
@@ -79,6 +81,7 @@ async fn main() -> io::Result<()> {
         .expect("GeoIP failure"),
     });
     info!("start background tasks");
+    // Setup databases renew task
     let ctx_ = ctx.clone();
     let renew_task = tokio::spawn(async {
         let mut interval = interval(Duration::from_days(1));
@@ -93,6 +96,7 @@ async fn main() -> io::Result<()> {
             }
         }
     });
+    // Setup analytics anonymization task
     let ctx_ = ctx.clone();
     let anonymize_task = tokio::spawn(async {
         let mut interval = interval(Duration::from_hours(1));
@@ -111,6 +115,22 @@ async fn main() -> io::Result<()> {
             }
         }
     });
+    // Setup rate limiting
+    let governor_conf = Arc::new(
+        GovernorConfigBuilder::default()
+            .per_second(1)
+            .burst_size(5)
+            .finish()
+            .unwrap(),
+    );
+    let governor_limiter = governor_conf.limiter().clone();
+    let rate_limit_task = tokio::spawn(async move {
+        let mut interval = interval(Duration::from_mins(1));
+        loop {
+            interval.tick().await;
+            governor_limiter.retain_recent();
+        }
+    });
     info!("start http server");
     let app = Router::new()
         .route("/health", get(route::health))
@@ -122,13 +142,17 @@ async fn main() -> io::Result<()> {
             post(route::newsletter::unsubscribe),
         )
         .layer(TraceLayer::new_for_http())
+        .layer(GovernorLayer {
+            config: governor_conf,
+        })
         .with_state(ctx);
     let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", config.port)).await?;
     select! {
         res = axum::serve(listener, app) => res.expect("HTTP failure"),
         res = connection => res.expect("Database failure"),
-        _ = renew_task => panic!("Resource renew failure"),
-        _ = anonymize_task => panic!("Anonymization failure"),
+        _ = renew_task => panic!("Resource renew task failure"),
+        _ = anonymize_task => panic!("Anonymization task failure"),
+        _ = rate_limit_task => panic!("Rate limiting task failure"),
     }
     Ok(())
 }
