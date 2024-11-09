8c4a6a93e9323b37cc04b6a5c248086aca0ade77
llenotre <llenotre@student.42.fr>
2024-11-09 14:40:56 +0100

    refactor: config

--- gateway-api/src/analytics.rs
+++ gateway-api/src/analytics.rs
@@ -1,10 +1,10 @@
 //! Analytics management.
 
-use crate::util;
+use crate::{util, Config};
 use axum::{
-    extract::{FromRequestParts, Request},
-    http::header::{REFERER, USER_AGENT},
-    response::Response,
+	extract::{FromRequestParts, Request},
+	http::header::{REFERER, USER_AGENT},
+	response::Response,
 };
 use axum_client_ip::InsecureClientIp;
 use chrono::{DateTime, Utc};
@@ -12,71 +12,49 @@ use futures::executor::block_on;
 use futures_util::future::BoxFuture;
 use serde::{Deserialize, Serialize};
 use std::{
-    env,
-    net::IpAddr,
-    sync::LazyLock,
-    task::{Context, Poll},
-    time::Duration,
+	net::IpAddr,
+	sync::Arc,
+	task::{Context, Poll},
+	time::Duration,
 };
 use tokio::{
-    select,
-    sync::mpsc::{unbounded_channel, UnboundedSender},
-    time::interval,
+	select,
+	sync::mpsc::{unbounded_channel, UnboundedSender},
+	time::interval,
 };
 use tower::{Layer, Service};
 use tracing::{error, info};
 
 const FLUSH_THRESHOLD: usize = 1024;
 
-/// Access pool configuration.
-struct AccessPoolConfig {
-    url: String,
-    property: String,
-    secret: String,
-}
-
-impl AccessPoolConfig {
-    /// Returns the configuration from the current environment.
-    ///
-    /// If the environment is invalid, the function panics.
-    fn from_env() -> Self {
-        Self {
-            url: env::var("GATEWAY_URL").expect("GATEWAY_URL must be set"),
-            property: env::var("GATEWAY_PROPERTY").expect("GATEWAY_PROPERTY must be set"),
-            secret: env::var("GATEWAY_SECRET").expect("GATEWAY_SECRET must be set"),
-        }
-    }
-}
-
 /// An access log, emitted when accessing an endpoint.
 #[derive(Clone, Deserialize, Serialize)]
 pub struct Access {
-    #[serde(with = "util::date_format")]
-    pub date: DateTime<Utc>,
-    pub peer_addr: Option<IpAddr>,
-    pub user_agent: Option<String>,
-    pub referer: Option<String>,
-    pub method: String,
-    pub uri: String,
+	#[serde(with = "util::date_format")]
+	pub date: DateTime<Utc>,
+	pub peer_addr: Option<IpAddr>,
+	pub user_agent: Option<String>,
+	pub referer: Option<String>,
+	pub method: String,
+	pub uri: String,
 }
 
 /// A pool containing accesses to be flushed.
 pub struct AccessPool {
-    sender: UnboundedSender<Access>,
+	sender: UnboundedSender<Access>,
 }
 
 impl AccessPool {
-    /// Creates a new pool
-    fn new() -> Self {
-        let config = AccessPoolConfig::from_env();
-        let (sender, mut receiver) = unbounded_channel::<Access>();
-        tokio::spawn(async move {
-            let mut interval = interval(Duration::from_secs(10));
-            let mut buf = Vec::with_capacity(FLUSH_THRESHOLD);
-            loop {
-                let max = FLUSH_THRESHOLD.saturating_sub(buf.len());
-                select! {
-					_ = interval.tick() => Self::flush(&mut buf, &config).await,
+	/// Creates a new pool
+	fn new() -> Self {
+		let (sender, mut receiver) = unbounded_channel::<Access>();
+		tokio::spawn(async move {
+			let mut interval = interval(Duration::from_secs(10));
+			let mut buf = Vec::with_capacity(FLUSH_THRESHOLD);
+			loop {
+				let max = FLUSH_THRESHOLD.saturating_sub(buf.len());
+				select! {
+					_ = interval.tick() => Self::flush(&mut buf).await,
 					len = receiver.recv_many(&mut buf, max) => {
 						// If the queue is closed, stop
 						if len == 0 {
@@ -84,132 +62,149 @@ impl AccessPool {
 						}
 						// If the buffer is full, flush
 						if buf.len() >= FLUSH_THRESHOLD {
-							Self::flush(&mut buf, &config).await;
+							Self::flush(&mut buf).await;
 						}
 					},
 					_ = tokio::signal::ctrl_c() => break,
 				}
-            }
-            drop(receiver);
-            // Attempt to flush remaining accesses in buffer before stopping
-            while !buf.is_empty() {
-                Self::flush(&mut buf, &config).await;
-                interval.tick().await;
-            }
-        });
-        Self {
-            sender,
-        }
-    }
+			}
+			drop(receiver);
+			// Attempt to flush remaining accesses in buffer before stopping
+			while !buf.is_empty() {
+				Self::flush(&mut buf).await;
+				interval.tick().await;
+			}
+		});
+		Self {
+			sender,
+		}
+	}
 
-    /// Pushes new access to the pool.
-    async fn push(&self, access: Access) {
-        // If the other side is closed, that means the server is stopping anyway
-        let _ = self.sender.send(access);
-    }
+	/// Pushes new access to the pool.
+	async fn push(&self, access: Access) {
+		// If the other side is closed, that means the server is stopping anyway
+		let _ = self.sender.send(access);
+	}
 
-    /// Flushes the pool's content, clearing it.
-    ///
-    /// If the pool could not be flushed, its content is kept for a future retry.
-    async fn flush(pool: &mut Vec<Access>, config: &AccessPoolConfig) {
-        if pool.is_empty() {
-            return;
-        }
-        info!(count = pool.len(), "attempt to flush accesses");
-        // HTTP request to push accesses
-        let client = reqwest::Client::new();
-        let res = client
-            .put(&config.url)
-            .basic_auth(&config.property, Some(&config.secret))
-            .json(pool)
-            .send()
-            .await;
-        let response = match res {
-            Ok(response) => response,
-            Err(error) => {
-                error!(config.url, %error, "access: HTTP call failure");
-                return;
-            }
-        };
-        let status = response.status();
-        if !status.is_success() {
-            error!(config.url, %status, "access: HTTP call failure");
-            return;
-        }
-        // Success: clear pool
-        info!("clear accesses pool");
-        pool.clear();
-    }
+	/// Flushes the pool's content, clearing it.
+	///
+	/// If the pool could not be flushed, its content is kept for a future retry.
+	async fn flush(pool: &mut Vec<Access>) {
+		if pool.is_empty() {
+			return;
+		}
+		info!(count = pool.len(), "attempt to flush accesses");
+		let config = Config::get();
+		let url = format!("{}/access", config.url);
+		// HTTP request to push accesses
+		let client = reqwest::Client::new();
+		let res = client
+			.put(url)
+			.basic_auth(&config.property, Some(&config.secret))
+			.json(pool)
+			.send()
+			.await;
+		let response = match res {
+			Ok(response) => response,
+			Err(error) => {
+				error!(config.url, %error, "access: HTTP call failure");
+				return;
+			}
+		};
+		let status = response.status();
+		if !status.is_success() {
+			error!(config.url, %status, "access: HTTP call failure");
+			return;
+		}
+		// Success: clear pool
+		info!("clear accesses pool");
+		pool.clear();
+	}
 }
 
-static ACCESS_POOL: LazyLock<AccessPool> = LazyLock::new(|| AccessPool::new());
-
 /// Analytics collection layer.
+///
+/// **Note**: This layer requires connection information. The following call is required on the
+/// router:
+///
+/// ```
+/// into_make_service_with_connect_info::<SocketAddr>()
+/// ```
 #[derive(Clone)]
-pub struct AnalyticsLayer;
+pub struct AnalyticsLayer {
+	pool: Arc<AccessPool>,
+}
+
+impl Default for AnalyticsLayer {
+	fn default() -> Self {
+		Self {
+			pool: Arc::new(AccessPool::new()),
+		}
+	}
+}
 
 impl<S> Layer<S> for AnalyticsLayer {
-    type Service = AnalyticsMiddleware<S>;
+	type Service = AnalyticsMiddleware<S>;
 
-    fn layer(&self, inner: S) -> Self::Service {
-        AnalyticsMiddleware { inner }
-    }
+	fn layer(&self, inner: S) -> Self::Service {
+		AnalyticsMiddleware {
+			inner,
+			pool: self.pool.clone(),
+		}
+	}
 }
 
 /// Middleware collecting analytics data.
 #[derive(Clone)]
 pub struct AnalyticsMiddleware<S> {
-    inner: S,
+	inner: S,
+	pool: Arc<AccessPool>,
 }
 
 impl<S> Service<Request> for AnalyticsMiddleware<S>
 where
-    S: Service<Request, Response = Response> + Send + 'static,
-    S::Future: Send + 'static,
+	S: Service<Request, Response = Response> + Send + 'static,
+	S::Future: Send + 'static,
 {
-    type Error = S::Error;
-    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
-    type Response = S::Response;
+	type Error = S::Error;
+	type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+	type Response = S::Response;
 
-    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
-    }
+	fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+		self.inner.poll_ready(cx)
+	}
 
-    fn call(&mut self, request: Request) -> Self::Future {
-        // Get peer address
-        let (mut parts, body) = request.into_parts();
-        // According to the crate's documentation, `InsecureClientIp` is fine for geolocation
-        let peer_addr = block_on(InsecureClientIp::from_request_parts(&mut parts, &()))
-            .expect("could not retrieve peer address")
-            .0;
-        let request = Request::from_parts(parts, body);
-        // Gather data
-        let user_agent = request
-            .headers()
-            .get(USER_AGENT)
-            .and_then(|ua| ua.to_str().ok())
-            .map(str::to_owned);
-        let referer = request
-            .headers()
-            .get(REFERER)
-            .and_then(|ua| ua.to_str().ok())
-            .map(str::to_owned);
-        let method = request.method().to_string();
-        let uri = request.uri().to_string();
-        let future = self.inner.call(request);
-        Box::pin(async move {
-            ACCESS_POOL
-                .push(Access {
-                    date: Utc::now(),
-                    peer_addr: Some(peer_addr),
-                    user_agent,
-                    referer,
-                    method,
-                    uri,
-                })
-                .await;
-            let response: Response = future.await?;
-            Ok(response)
-        })
-    }
+	fn call(&mut self, request: Request) -> Self::Future {
+		// Get peer address
+		let (mut parts, body) = request.into_parts();
+		// According to the crate's documentation, `InsecureClientIp` is fine for geolocation
+		let peer_addr = block_on(InsecureClientIp::from_request_parts(&mut parts, &()))
+			.ok()
+			.map(|addr| addr.0);
+		let request = Request::from_parts(parts, body);
+		// Gather data
+		let access = Access {
+			date: Utc::now(),
+			peer_addr,
+			user_agent: request
+				.headers()
+				.get(USER_AGENT)
+				.and_then(|ua| ua.to_str().ok())
+				.map(str::to_owned),
+			referer: request
+				.headers()
+				.get(REFERER)
+				.and_then(|ua| ua.to_str().ok())
+				.map(str::to_owned),
+			method: request.method().to_string(),
+			uri: request.uri().to_string(),
+		};
+		let pool = self.pool.clone();
+		let future = self.inner.call(request);
+		Box::pin(async move {
+			pool.push(access).await;
+			let response: Response = future.await?;
+			Ok(response)
+		})
+	}
 }
--- gateway-api/src/lib.rs
+++ gateway-api/src/lib.rs
@@ -1,16 +1,44 @@
 //! API for the gateway and various utilities for HTTP services.
 
+use serde::Deserialize;
 use std::sync::OnceLock;
 
 pub mod analytics;
 pub mod util;
 
+/// Configuration for the API.
+#[derive(Deserialize)]
+pub struct Config {
+	/// URL to the gateway.
+	pub url: String,
+	/// The property's UUID.
+	pub property: String,
+	/// The property's secret.
+	pub secret: String,
+
+	/// The current service's hostname.
+	pub host: String,
+}
+
+impl Config {
+	/// Returns the configuration from the current environment.
+	///
+	/// If the configuration is incorrect, the function panics.
+	pub fn get() -> &'static Self {
+		static CONFIG: OnceLock<Config> = OnceLock::new();
+		CONFIG.get_or_init(|| {
+			envy::prefixed("GATEWAY_")
+				.from_env()
+				.expect("configuration")
+		})
+	}
+}
+
 /// Returns the content of the `robots.txt` file.
-///
-/// `host` is the hostname of the service.
-pub fn robots(host: &str) -> &'static str {
-    static ROBOTS: OnceLock<String> = OnceLock::new();
-    ROBOTS.get_or_init(|| {
-        format!(include_str!("robots.txt"), host)
-    })
+pub fn robots() -> &'static str {
+	static ROBOTS: OnceLock<String> = OnceLock::new();
+	ROBOTS.get_or_init(|| {
+		let config = Config::get();
+		format!(include_str!("robots.txt"), config.host)
+	})
 }
